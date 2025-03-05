ed584ac0a272da5a834bae0112685f740619a7dc
Sky1sBloo <ultimate8gaming@gmail.com>
2025-03-05 20:34:47 +0800

    Made project a library

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using VZ_Sky;
-
-string SERVER_IP = "127.0.0.1";
-int SERVER_PORT = 10809;
-VzConnection connection = new VzConnection(SERVER_IP, SERVER_PORT);
-TestProgram testProgram = new TestProgram(connection);
-
-await Task.Delay(-1);
--- VzProgram.cs
+++ VzProgram.cs
@@ -1,54 +1,55 @@
-using VZ_Sky;
-
-
-/// <summary>
-/// Class representing a Vizzy Program
-/// Use this to automatically handle receiving data
-/// and easily allocate async funtions
-/// </summary>
-public abstract class VzProgram
+namespace VZ_Sky
 {
-    // Call this if you need to send or receive data
-    protected VzConnection Connection { get; private set; }
-
-    public VzProgram(VzConnection connection)
+    /// <summary>
+    /// Class representing a Vizzy Program
+    /// Use this to automatically handle receiving data
+    /// and easily allocate async funtions
+    /// </summary>
+    public abstract class VzProgram
     {
-        this.Connection = connection;
-        startAsyncTask(receiveData);
-        startAsyncTask(OnStart);
-    }
+        // Call this if you need to send or receive data
+        protected VzConnection Connection { get; private set; }
 
-
-    /// <summary>
-    /// Automatically called on start of program
-    /// Runs on a separate thread to allow for delay
-    /// (Ensure that it is async function)
-    /// </summary>
-    public abstract Task OnStart();
-
-    /// <summary>
-    /// Gets called when program receives a data
-    /// </summary>
-    public abstract void ReceivedData(List<VzType> values);
-
-    /// <summary>
-    /// Create a asynchronous task (generally used onStart functions)
-    /// </summary>
-    public void startAsyncTask(Func<Task> callback)
-    {
-        Task.Run(callback);
-    }
-
-    /// <summary>
-    /// Function call to call ReceivedData when it receives a message
-    /// </summary>
-    private async Task receiveData()
-    {
-        while (true)
+        public VzProgram(VzConnection connection)
         {
-            List<VzType> values = await Connection.ReceiveDataAsync();
-            ReceivedData(values);
+            this.Connection = connection;
+            startAsyncTask(receiveData);
+            startAsyncTask(OnStart);
+        }
+
+
+        /// <summary>
+        /// Automatically called on start of program
+        /// Runs on a separate thread to allow for delay
+        /// (Ensure that it is async function)
+        /// </summary>
+        public abstract Task OnStart();
+
+        /// <summary>
+        /// Gets called when program receives a data
+        /// </summary>
+        public abstract void ReceivedData(List<VzType> values);
+
+        /// <summary>
+        /// Create a asynchronous task (generally used onStart functions)
+        /// </summary>
+        public void startAsyncTask(Func<Task> callback)
+        {
+            Task.Run(callback);
+        }
+
+        /// <summary>
+        /// Function call to call ReceivedData when it receives a message
+        /// </summary>
+        private async Task receiveData()
+        {
+            while (true)
+            {
+                List<VzType> values = await Connection.ReceiveDataAsync();
+                ReceivedData(values);
+            }
         }
     }
+
 }
 
